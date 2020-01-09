55e8c8554f3d4d4ed298bcb0dea92db71d41d626
emukungu <enmukungu@gmail.com>
2020-01-09 11:54:05 +0300

    feat(iot): read data from an iot device. (#1)

--- iot_project/urls.py
+++ iot_project/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('authentication.urls', namespace='user_auth')),
+    path('iot/', include('iot_weather.urls', namespace='iot_weather')),
+    path('', TemplateView.as_view(template_name='base_layout.html'), name='home')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
